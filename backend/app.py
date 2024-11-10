b99ca80e55c8745a607c07236528f9a77170715d
mayanksharma52 <mayank.sharma1_cs22@gla.ac.in>
2024-11-10 21:04:41 +0530

    attatch database sqlalchemy and make todo app functional

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.instance_path, 'Todo.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/')
+db = SQLAlchemy(app)
+
+class ToDo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+@app.route('/',methods=['GET','POST'])
 def home():
-    return render_template('index.html')
+    if request.method=='POST':
+        title=request.form['title']
+        desc=request.form['desc']
+        todo = ToDo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+
+
+    allTodo=ToDo.query.all()
+
+
+    return render_template('index.html',allTodo=allTodo)
+
 
 if __name__ == '__main__':
     app.run(debug=True)

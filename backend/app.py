069a66b61797beed903212cb25f606e30b2f081c
mayanksharma52 <mayank.sharma1_cs22@gla.ac.in>
2024-11-11 15:40:48 +0530

    learb inheritance template and add functionality to uodate button

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request ,redirect
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import os
@@ -18,27 +18,34 @@ class ToDo(db.Model):
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method=='POST':
-        title=request.form['title']
-        desc=request.form['desc']
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
         todo = ToDo(title=title, desc=desc)
         db.session.add(todo)
         db.session.commit()
-
-
-    allTodo=ToDo.query.all()
-
-
-    return render_template('index.html',allTodo=allTodo)
+    allTodo = ToDo.query.all()
+    return render_template('index.html', allTodo=allTodo)
 
 @app.route('/delete/<int:sno>')
 def delete(sno):
-    todo=ToDo.query.filter_by(sno=sno).first()
+    todo = ToDo.query.filter_by(sno=sno).first()
     db.session.delete(todo)
     db.session.commit()
     return redirect('/')
 
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    todo = ToDo.query.filter_by(sno=sno).first()
+    if request.method == 'POST':
+        todo.title = request.form['title']
+        todo.desc = request.form['desc']
+        db.session.commit()
+        return redirect('/')
+    return render_template('update.html', todo=todo)
+
 if __name__ == '__main__':
+    os.makedirs(app.instance_path, exist_ok=True)  # Create the instance folder if it doesn't exist
     app.run(debug=True)
